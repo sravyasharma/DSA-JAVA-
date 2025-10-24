e252291c31f5820450772b85fe71c1bc6a4b5ce8
Sravya <sravyadevallalaxmi@gmail.com>
2025-10-24 22:14:58 +0530

    Time: 6 ms (6.14%), Space: 44.6 MB (58.23%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -22,13 +22,10 @@ class Solution {
         Queue<TreeNode> q=new LinkedList<>();
         q.offer(root);
         while(!q.isEmpty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
                 TreeNode node=q.poll();
                 res.add(node.val);
                 if(node.left!=null) q.offer(node.left);
                 if(node.right!=null) q.offer(node.right);
-            }
         }
         Collections.sort(res);
         int min=Integer.MAX_VALUE;
