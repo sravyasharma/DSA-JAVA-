f2d8970f9cb82365cc046dab0fc846eed77f19c6
Sravya <sravyadevallalaxmi@gmail.com>
2025-12-12 13:29:01 +0530

    Time: 1 ms (99.76%), Space: 46.2 MB (57.28%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote.length()>magazine.length()) return false;
+        int[] charac=new int[26];
+        for(char c:magazine.toCharArray()){
+            charac[c-'a']++;
+        }
+        for(char c:ransomNote.toCharArray()){
+            if(charac[c-'a']==0) return false;
+            charac[c-'a']--;
+        }
+        return true;
+    }
+}
