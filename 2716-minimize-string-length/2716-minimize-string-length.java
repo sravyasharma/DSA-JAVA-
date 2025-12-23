9ab7c4d92f7790762c134ba2b8e44051a64c7228
Sravya <sravyadevallalaxmi@gmail.com>
2025-12-24 00:22:51 +0530

    Time: 9 ms (18.6%), Space: 47 MB (29.84%) - LeetHub

---
+++ 2716-minimize-string-length/2716-minimize-string-length.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+        Map<Character,Integer> map=new HashMap<>();
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        return map.size();
+    }
+}
