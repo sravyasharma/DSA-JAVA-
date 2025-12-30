74a03b6c3de601eae1b1e6704bba91c878173fc8
Sravya <sravyadevallalaxmi@gmail.com>
2025-12-31 02:32:02 +0530

    Time: 1 ms (100%), Space: 42.8 MB (21.89%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,22 +1,3 @@
-/*class Solution {
-    public int arrangeCoins(int n) {
-        /*int left=1;
-        int right=n;
-        int ans=0;
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            int coins=(mid*(mid+1))/2;
-            if(coins==n) ans=(int) mid;
-            else if(coins<n){
-                ans=(int)mid;
-                left=mid+1;
-            }
-            else right=mid-1;
-        }
-        return ans;
-        
-    }
-}*/
 class Solution {
     public int arrangeCoins(int n) {
         long left=1, right=n;int res=0;
