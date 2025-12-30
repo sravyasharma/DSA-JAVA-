f818500e50f150dc1bb6be09d0be4933320d2c7e
Sravya <sravyadevallalaxmi@gmail.com>
2025-12-31 02:16:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -3,15 +3,16 @@ class Solution {
         int left=1;
         int right=n;
         int ans=0;
-        while(left<right){
+        while(left<=right){
             int mid=left+(right-left)/2;
             int coins=(mid*(mid+1))/2;
-            if(coins<=n){
+            if(coins==n) ans=(int) mid;
+            else if(coins<n){
                 ans=(int)mid;
                 left=mid+1;
             }
             else right=mid-1;
         }
-        return (int)left;
+        return ans;
     }
 }
