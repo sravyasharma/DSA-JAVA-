252f46805fa5c78bbea1e3a2526fd2c7be02be3f
Sravya <sravyadevallalaxmi@gmail.com>
2025-12-31 22:38:00 +0530

    Time: 30 ms (43.53%), Space: 92.2 MB (11.44%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -1,4 +1,4 @@
-/*class Solution {
+class Solution {
     public int maximumCandies(int[] candies, long k) {
         int max=Arrays.stream(candies).max().getAsInt();
         int left=1;
@@ -6,7 +6,7 @@
         int ans=0;
         while(left<=right){
             int mid=left+(right-left)/2;
-            int count=0;
+            long count=0;
             for(int candie:candies){
                 count+=candie/mid;
             }
@@ -16,30 +16,6 @@
             }
             else{
                 right=mid-1;
-            }
-        }
-        return ans;
-    }
-}*/
-class Solution {
-    public int maximumCandies(int[] candies, long k) {
-        int max = Arrays.stream(candies).max().getAsInt();
-        int left = 1, right = max;
-        int ans = 0;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            long count = 0;
-
-            for (int c : candies) {
-                count += c / mid;
-            }
-
-            if (count >= k) {
-                ans = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
             }
         }
         return ans;
