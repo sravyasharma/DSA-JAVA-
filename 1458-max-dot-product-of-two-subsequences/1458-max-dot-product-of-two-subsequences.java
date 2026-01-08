bcc892de80c1678038e996ce93728d444d327c25
Sravya <sravyadevallalaxmi@gmail.com>
2026-01-08 23:12:06 +0530

    Time: 15 ms (24.44%), Space: 46.7 MB (31.85%) - LeetHub

---
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+        int m=nums1.length;
+        int n=nums2.length;
+        int[][] dp=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int prod=nums1[i]*nums2[j];
+                dp[i][j]=prod;
+                if(i>0 && j>0){
+                    dp[i][j]=Math.max(dp[i][j],prod+Math.max(0,dp[i-1][j-1]));
+                }
+                if(i>0){
+                    dp[i][j]=Math.max(dp[i][j],dp[i-1][j]);
+                }
+                if(j>0){
+                    dp[i][j]=Math.max(dp[i][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
