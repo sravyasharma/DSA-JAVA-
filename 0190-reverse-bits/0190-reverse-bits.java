ab0024a545a1266852e472e8e29dd16cd44d7652
Sravya <sravyadevallalaxmi@gmail.com>
2026-02-16 18:12:49 +0530

    Time: 0 ms (100%), Space: 42.6 MB (34.47%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,5 +1,10 @@
 class Solution {
     public int reverseBits(int n) {
-        return Integer.reverse(n);
+        int res=0;
+        for(int i=0;i<32;i++){
+            res= (res<<1) | (n&1);
+            n>>>=1;
+        }
+        return res;
     }
 }
